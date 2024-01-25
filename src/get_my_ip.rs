f64896794e1a4440a22a412bbbfd004240ca12a9
simon-fu <simonok@139.com>
2024-01-25 13:09:17 +0800

    use ipv4 when get ip

--- src/get_my_ip.rs
+++ src/get_my_ip.rs
@@ -1,4 +1,4 @@
-use std::net::IpAddr;
+use std::net::{IpAddr, Ipv4Addr};
 
 use anyhow::Result;
 use serde_derive::Deserialize;
@@ -12,10 +12,23 @@ pub async fn get_my_ip() -> Result<String> {
 async fn get_ip_jsonip() -> Result<String> {
     const URL: &str = "http://jsonip.com";
 
-    let body = reqwest::get(URL)
-    .await?
-    .text()
-    .await?;
+    let bind_addr = IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0));
+    let client = reqwest::Client::builder()
+        .local_address(bind_addr) 
+        .build()?;
+
+    let body = client
+        .get(URL)
+        .send()
+        .await?
+        .text()
+        .await?;
+
+
+    // let body = reqwest::get(URL)
+    // .await?
+    // .text()
+    // .await?;
 
     let rsp: IpResponse = serde_json::from_str(body.as_str())?;
     check_ip(&rsp.ip)?;
